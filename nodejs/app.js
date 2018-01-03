c372b1b33e3f01c8d3910babd5400129965a3b33
Michael Ringholm Sundgaard <michaelringholm@gmail.com>
2018-01-03 22:33:52 +0100

    2nd checkin

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,17 +1,30 @@
 var express = require('express');
 var app = express();
 var fs = require("fs");
+bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
 
 app.get('/listUsers', function (req, res) {
-   fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
-       console.log( data );
-       res.end( data );
-   });
-})
+   //fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+   //    console.log( data );
+   //    res.end( data );
+   //});
+});
 
 app.get('/', function(req, res) {
-	res.end('Getting something....');
-})
+	res.end('{"description":"sports-service 1.0"}');
+});
+
+app.get('/football/uk/pl', function(req, res) {
+        res.end('{"homeTeam":"Manchester United", "awayTeam":"Liverpool", "matchStartTime":"06.02.2018 15:00", "status":"not started"}');
+});
+
+app.put('/football/uk/pl', function(req, res) {
+	console.log("Adding new match to schedule...");
+	console.log("Got " + JSON.stringify(req.body));
+        res.end('{"status":"match added!"}');
+});
 
 var server = app.listen(8081, function () {
 
